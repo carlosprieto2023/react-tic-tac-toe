2ed2b58ac15c2661d42300148031d61dac5e8db2
carlosdos818@yahoo.com <carlosdos818@yahoo.com>
2024-09-11 18:32:57 -0400

    Adds winning combinations,checks for winner and strikes

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,67 +1,66 @@
-
-
 import Tile from "./Tile"
+import Strike from "../Strike"
 
 
+export default function Board({ tiles, onTileClick, playerTurn, strikeClass }) {
 
-export default function Board({ tiles, onTileClick, playerTurn }) {
-    
 
-    return(
+    return (
         <div className="board-container">
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(0)} 
-                value={tiles[0]} className="border-right border-bottom tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(0)}
+                value={tiles[0]} className="border-right border-bottom tile-container"
             />
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(1)} 
-                value={tiles[1]} 
-                className="border-right border-bottom tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(1)}
+                value={tiles[1]}
+                className="border-right border-bottom tile-container"
             />
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(2)} 
-                value={tiles[2]} 
-                className="border-bottom tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(2)}
+                value={tiles[2]}
+                className="border-bottom tile-container"
             />
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(3)} 
-                value={tiles[3]} 
-                className="border-right border-bottom tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(3)}
+                value={tiles[3]}
+                className="border-right border-bottom tile-container"
             />
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(4)} 
-                value={tiles[4]} 
-                className="border-right border-bottom tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(4)}
+                value={tiles[4]}
+                className="border-right border-bottom tile-container"
             />
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(5)} 
-                value={tiles[5]} 
-                className="border-bottom tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(5)}
+                value={tiles[5]}
+                className="border-bottom tile-container"
             />
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(6)} 
-                value={tiles[6]} 
-                className="border-right tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(6)}
+                value={tiles[6]}
+                className="border-right tile-container"
             />
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(7)} 
-                value={tiles[7]} 
-                className="border-right tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(7)}
+                value={tiles[7]}
+                className="border-right tile-container"
             />
-            <Tile 
-                playerTurn={playerTurn} 
-                onClick={() => onTileClick(8)} 
-                value={tiles[8]} 
-                className="tile-container" 
+            <Tile
+                playerTurn={playerTurn}
+                onClick={() => onTileClick(8)}
+                value={tiles[8]}
+                className="tile-container"
             />
+            <Strike strikeClass={strikeClass} />
         </div>
     )
 }
